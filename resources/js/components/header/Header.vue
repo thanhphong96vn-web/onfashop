0918655f23057f914525170d4d47d3e702dfb616
Thanh Tho <lethanhtho1953@gmail.com>
2025-12-25 01:03:42 +0700

    Sủa header

--- resources/js/components/header/Header.vue
+++ resources/js/components/header/Header.vue
@@ -1,19 +1,16 @@
 <template>
-  <header :class="['header-sticky ', { 'sticky-top': generalSettings.sticky_header == 1 }] ">
-    <TopBar
-      :loading="loading"
-      :data="data"
-    />
-    <LogoBar
-      :loading="loading"
-      :data="data"
-    />
-    <HeaderMenu
-      :loading="loading"
-      :data="data"
-      class="d-none d-md-block"
-    />
-  </header>
+    <header
+        :class="[
+            'header-sticky ',
+            { 'sticky-top': generalSettings.sticky_header == 1 },
+        ]"
+    >
+        <TopBar :loading="loading" :data="data" />
+        <v-divider></v-divider>
+        <LogoBar :loading="loading" :data="data" />
+        <v-divider></v-divider>
+        <HeaderMenu :loading="loading" :data="data" />
+    </header>
 </template>
 
 <script>
@@ -22,30 +19,30 @@ import HeaderMenu from "./HeaderMenu.vue";
 import LogoBar from "./LogoBar.vue";
 import TopBar from "./TopBar.vue";
 export default {
-  data: () => ({
-    loading: true,
-    data: {},
-  }),
-  components: {
-    TopBar,
-    LogoBar,
-    HeaderMenu,
-  },
-  computed: {
-    ...mapGetters("app", ["generalSettings"]),
-  },
-  methods: {
-    async getDetails() {
-      const res = await this.call_api("get", `setting/header`);
-
-      if (res.status === 200) {
-        this.data = res.data;
-        this.loading = false;
-      }
+    data: () => ({
+        loading: true,
+        data: {},
+    }),
+    components: {
+        TopBar,
+        LogoBar,
+        HeaderMenu,
     },
-  },
-  created() {
-    this.getDetails();
-  },
+    computed: {
+        ...mapGetters("app", ["generalSettings"]),
+    },
+    methods: {
+        async getDetails() {
+            const res = await this.call_api("get", `setting/header`);
+
+            if (res.status === 200) {
+                this.data = res.data;
+                this.loading = false;
+            }
+        },
+    },
+    created() {
+        this.getDetails();
+    },
 };
 </script>
